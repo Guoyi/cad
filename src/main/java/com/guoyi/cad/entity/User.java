d27a1014f05c5c929900ccccbc5d1e152ad07b04
kzhang <guoyi.zhang@gmail.com>
2014-05-09 14:33:08 +0800

    add bootstrap 3 into the project.

--- src/main/java/com/guoyi/cad/entity/User.java
+++ src/main/java/com/guoyi/cad/entity/User.java
@@ -1,19 +1,34 @@
 package com.guoyi.cad.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 @Entity
-public class User extends AbstractPersistable<Long>{
-	
+public class User extends AbstractPersistable<Long> {
+
 	private static final long serialVersionUID = 1L;
 
 	private String userName;
-	
+
+	private String password;
+
 	private String firstName;
-	
+
 	private String lastName;
+
+	private String email;
+
+	@Column(length = 2000)
+	private String address;
+
+	@ManyToOne(fetch= FetchType.LAZY)
+	@JoinColumn(name = "role_id")
+	private Role role;
 
 	public String getUserName() {
 		return userName;
@@ -38,8 +53,37 @@ public class User extends AbstractPersistable<Long>{
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	
-	
 
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
 
 }
